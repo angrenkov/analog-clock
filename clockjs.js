350412354b25db19bea7701128e9ee42fdd35968
Anton <a_grenkov@yahoo.com>
2018-07-21 13:57:07 +0300

    Bugs were fixed. Added setting time on click.

--- clockjs.js
+++ clockjs.js
@@ -1,15 +1,15 @@
-function displayCanvas() {
+function displayCanvas(d) {
 
-    var canvas = document.getElementById("drawingCanvas"),
-        context = canvas.getContext("2d"),
-        radiusClock = canvas.width / 2 - 10,
-        xCenterClock = canvas.width / 2,
-        yCenterClock = canvas.height / 2,
-        radiusNum = radiusClock - 10,
-        lengthSeconds = radiusNum - 15,
-        lengthMinutes = radiusNum - 20,
-        lengthHour = lengthMinutes / 1.5,
-        img = new Image();
+    let canvas = document.getElementById("drawingCanvas");
+    let context = canvas.getContext("2d");
+    let radiusClock = canvas.width / 2 - 10;
+    let xCenterClock = canvas.width / 2;
+    let yCenterClock = canvas.height / 2;
+    let radiusNum = radiusClock - 10;
+    let lengthSeconds = radiusNum - 15;
+    let lengthMinutes = radiusNum - 20;
+    let lengthHour = lengthMinutes / 1.5;
+    let img = new Image();
 
     img.src = 'images/clock3.png';
     img.onload = function () {
@@ -19,65 +19,102 @@ function displayCanvas() {
     context.fillStyle = "#D3D3D3";
     context.fillRect(0,0,canvas.width,canvas.height);
 
-    // Центр
+    // Drawing center
 
     context.beginPath();
     context.strokeStyle =  "#000000";
     context.fillStyle = "#ffffff";
     context.lineWidth = 3;
-    context.arc(xCenterClock, yCenterClock, 5, 0, 2*Math.PI, true);
+    context.arc(xCenterClock, yCenterClock, 5, 0, 2 * Math.PI, true);
     context.stroke();
     context.fill();
     context.closePath();
 
-    var d = new Date();
+    let tSec = 6 * new Date().getSeconds();
+    let tMin = (!d) ? 6 * ((new Date().getMinutes()) + (1 / 60) * new Date().getSeconds()) : 6 * ((d.getMinutes()) + (1 / 60) * new Date().getSeconds());
+    let tHour = (!d) ? 30 * ((new Date().getHours()) + (1 / 60) * (new Date().getMinutes())) : 30 * ((d.getHours()) + (1 / 60) * (d.getMinutes()));
 
-    var t_sec = 6*d.getSeconds();
-    var t_min = 6*(d.getMinutes() + (1/60)*d.getSeconds());
-    var t_hour = 30*(d.getHours() + (1/60)*d.getMinutes());
-
-    // Секунды
+    // Drawing seconds hand
 
     context.beginPath();
     context.strokeStyle =  "#FF0000";
     context.moveTo(xCenterClock, yCenterClock);
-    context.lineTo(xCenterClock + lengthSeconds*Math.cos(Math.PI/2 - t_sec*(Math.PI/180)),
-        yCenterClock - lengthSeconds*Math.sin(Math.PI/2 - t_sec*(Math.PI/180)));
+    context.lineTo(xCenterClock +
+        lengthSeconds *
+        Math.cos(Math.PI / 2 - tSec * (Math.PI / 180)),
+        yCenterClock -
+        lengthSeconds *
+        Math.sin(Math.PI / 2 - tSec * (Math.PI / 180)));
     context.stroke();
     context.closePath();
 
-    // Минуты
+    // Drawing minutes hand
 
     context.beginPath();
     context.strokeStyle =  "#000000";
     context.lineWidth = 3;
     context.moveTo(xCenterClock, yCenterClock);
-    context.lineTo(xCenterClock + lengthMinutes*Math.cos(Math.PI/2 - t_min*(Math.PI/180)),
-        yCenterClock - lengthMinutes*Math.sin(Math.PI/2 - t_min*(Math.PI/180)));
+    context.lineTo(xCenterClock +
+        lengthMinutes *
+        Math.cos(Math.PI / 2 - tMin * (Math.PI / 180)),
+        yCenterClock -
+        lengthMinutes *
+        Math.sin(Math.PI / 2 - tMin * (Math.PI / 180)));
     context.stroke();
     context.closePath();
 
-    // Часы
+    // Drawing hours hand
 
     context.beginPath();
     context.lineWidth = 5;
     context.moveTo(xCenterClock, yCenterClock);
-    context.lineTo(xCenterClock + lengthHour*Math.cos(Math.PI/2 - t_hour*(Math.PI/180)),
-        yCenterClock - lengthHour*Math.sin(Math.PI/2 - t_hour*(Math.PI/180)));
+    context.lineTo(xCenterClock +
+        lengthHour *
+        Math.cos(Math.PI / 2 - tHour * (Math.PI / 180)),
+        yCenterClock -
+        lengthHour *
+        Math.sin(Math.PI / 2 - tHour * (Math.PI / 180)));
     context.stroke();
     context.closePath();
 
     return;
 }
 
+let i;
+let d;
+
 window.onload = function(){
-    window.setInterval(
-        function(){
-            displayCanvas();
-        }
-        , 1000);
+    displayCanvas();
+    i = setInterval(displayCanvas, 1000);
 }
 
-function settingTime(){
-    var setTime = document.getElementById('nTime').value;
+function setTime(){
+    let setTime = document.getElementById('nTime').value;
+    setTime = {hours: setTime.substr(0, 2), minutes: setTime.substr(3, 2)};
+    d = new Date();
+    d.setHours(setTime.hours);
+    d.setMinutes(setTime.minutes);
+    setArrows(d);
 }
+
+function setArrows(d){
+    clearInterval(i);
+    displayCanvas(d);
+    function f() {
+        if (new Date().getSeconds() === 0) {
+            let m = this.getMinutes();
+            m++;
+            this.setMinutes(m);
+            if (m === 0) {
+                let h = this.getHours();
+                h++;
+                this.setHours(h);
+            }
+        }
+        displayCanvas(this);
+    }
+    x = f.bind(d);
+    i = setInterval(x, 1000);
+}
+
+
